ba627bf1ea160b36a921acdd737ac9e69cce3f4c
github-xuanwang <35718382+github-xuanwang@users.noreply.github.com>
2025-03-24 12:30:34 -0400

    Update CORS configuration to allow requests from GitHub Pages

--- musicapp-server/server.js
+++ musicapp-server/server.js
@@ -39,14 +39,14 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "X-Requested-With,content-type,Authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
