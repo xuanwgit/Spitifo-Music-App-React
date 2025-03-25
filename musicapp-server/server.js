b5c366db0e6cb337517eddf055a176118c9bce3f
github-xuanwang <35718382+github-xuanwang@users.noreply.github.com>
2025-03-25 00:43:55 -0400

    Fix CORS configuration

--- musicapp-server/server.js
+++ musicapp-server/server.js
@@ -36,24 +36,11 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000', 'https://xuanwgit.github.io'],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.urlencoded({ extended: true }));
-
-// CORS configuration
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 // Request logging middleware
 app.use((req, res, next) => {
